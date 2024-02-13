9fe74f5fc180781a3ebc32de86f4acf2d7983253
mdmahbubekhuda <md.mahbub.e@gmail.com>
2024-02-13 10:04:31 +0600

    set admin and remove user api created

--- index.js
+++ index.js
@@ -32,6 +32,12 @@ async function run() {
         // users
         const userCollection = client.db('bistroDB').collection('users')
 
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray()
+            res.send(result)
+        })
+
+
         app.post('/users', async (req, res) => {
             const user = req.body
             const query = { email: user.email }
@@ -41,6 +47,24 @@ async function run() {
             res.send(result)
         })
 
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc)
+            res.send(result)
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+        })
 
         // cart
         const cartCollection = client.db("bistroDB").collection('carts')
