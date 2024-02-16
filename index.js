07e6dc08d1c9cbbed6a6d50d7a9800ba30eaf3f8
mdmahbubekhuda <md.mahbub.e@gmail.com>
2024-02-17 04:10:54 +0600

    vercel

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ async function run() {
         app.post('/jwt', (req, res) => {
             const user = req.body
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-            res.cookie('access-token', token, { httpOnly: true, secure: true, sameSite: 'none' }).send({ success: true })
+            res.cookie('access-token', token, { httpOnly: true, secure: false, sameSite: 'lax' }).send({ success: true })
         })
 
         // jwt - remove access token
